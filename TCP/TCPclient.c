a48261d06aea5b9aabe716fc9b087dfa0c3fbbdf
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2018-03-14 16:13:56 +0530

    improvements

---
+++ TCP/TCPclient.c
@@ -0,0 +1,60 @@
+/*
+Creating the TCP Client workflow in this program using C.
+
+ * Title : TCP client
+ * Name : Aditya Pratap Singh Rajput
+ * Subject : Network Protocols And Programming
+
+Note : please consider the TYPOS in comments.
+Thanks.
+*/
+//adding includes
+#include <stdio.h>
+#include <stdlib.h>
+//for socket and related functions
+#include <sys/types.h>
+#include <sys/socket.h>
+//for including structures which will store information needed
+#include <netinet/in.h>
+
+#include <unistd.h>
+//main functions
+int main(){
+  //creating a socket
+  int network_socket;
+  //calling socket function and storing the result in the variable
+  //socket(domainOfTheSocket,TypeOfTheSocket,FlagForProtocol{0 for default protocol i.e, TCP})
+  //AF_INET = constant defined in the header file for us
+  //TCP vs UDP --- SOCK_STRAM for TCP
+  // flag 0 for TCP (default protocol)
+  network_socket = socket(AF_INET,SOCK_STREAM,0);
+  //creating network connection
+  //in order to connect to the other side of the socket we need to declare connect
+  //with specifying address where we want to connect to
+  //already defined struct sockaddr_in
+  struct sockaddr_in server_address;
+  //what type of address we are woking with
+  server_address.sin_family = AF_INET;
+//for taking the port number and htons converts the port # to the appropriate data type we want to write
+//to specifying the port
+//htons : conversion functions
+  server_address.sin_port = htons(9002);
+  //structure within structure A.B.c
+  server_address.sin_addr.s_addr = INADDR_ANY;//INADDR_ANY is for connection with 0000
+//connect returns us a response that connection is establlised or not
+int connect_status =  connect(network_socket,(struct sockaddr *) &server_address, sizeof(server_address));
+//check for the error with the connection
+if (connect_status == -1){
+  printf("There was an error making a connection to socket\n" );
+}
+//recieve data from the server
+char server_response[256];
+//recieve the data from the server
+recv(network_socket,&server_response,sizeof(server_response),0);
+//recieved data from the server successfully then printing the data obtained from the server
+
+printf("Ther server sent the data : %s",server_response);
+//closing the socket
+close(network_socket);
+  return 0;
+}
--- date_time/server.c
+++ date_time/server.c
@@ -6,46 +6,47 @@
 #include<netinet/in.h>
 #include<unistd.h>
 #include<time.h>
-// time
-
-
+//port value and the max size for buffer
+#define PORT 9002 // the port users will be connecting to
 #define MAXLINE 1024
-
+#define BACKLOG 10 // how many pending connections queue will hold
 int main(){
-
+  //*******************Time Setup***********************
+  int counter=0;
   //for showing the time from the server to the client
   time_t time_from_pc;
-  struct tm * timeinfo;
-  time(&time_from_pc);
-  timeinfo = localtime (&time_from_pc);
+  // struct tm * timeinfo;
+  // time(&time_from_pc);
+  // timeinfo = localtime (&time_from_pc);
   //same as echo server
+  //****************************************************
   int socket_descriptor;
   int n;
   socklen_t length;
   char msg[MAXLINE];
   struct sockaddr_in  servaddr,cliaddr;
 
-  socket_descriptor = socket(AF_INET,SOCK_DGRAM,0);
+  socket_descriptor = socket(AF_INET,SOCK_STREAM,0);
 
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr=INADDR_ANY;
-  servaddr.sin_port=htons(5035);
+  servaddr.sin_port=htons(PORT);
 
-  bind(socket_descriptor,(struct sockaddr*)&servaddr,sizeof(servaddr));
-
+  if(bind(socket_descriptor,(struct sockaddr*)&servaddr,sizeof(servaddr))==-1){
+    perror("bind");
+    exit(0);
+  }
+  listen(socket_descriptor,BACKLOG);
   printf("\nServer Started ...");
 while(1){
     printf("\n");
     length = sizeof(cliaddr);
+int client_socket;
+client_socket=accept(socket_descriptor,NULL,NULL);
 
-    n = recvfrom(socket_descriptor,msg,MAXLINE,0,(struct sockaddr*)&cliaddr,&length);
+    printf("\n client has requested for time at %s",ctime(time_from_pc));
 
-    printf("\n %s has requested for time at %s",msg,asctime(timeinfo));
-
-    if(n<6)
-      perror("send error");
-
-      sendto(socket_descriptor,asctime(timeinfo),n,0,(struct sockaddr*)&cliaddr,length);
+      send(client_socket,ctime(&time_from_pc),30,0);
   }
   return 0;
 }
