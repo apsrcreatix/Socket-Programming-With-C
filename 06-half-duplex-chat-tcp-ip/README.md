8c51cbb824fac18fabeecf0181ccde75bf7a9cc3
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2018-04-27 03:12:17 +0530

    improved half duplex

---
+++ 06-half-duplex-chat-tcp-ip/README.md
@@ -0,0 +1,5 @@
+# Instructions
+
+* If you don't have knowledge of basics of TCP, please check previous topics ```03-tcp-ip-client-server```
+* We are preparing half duplex communication method
+* ***Half Duplex*** : Refers to the transmission of data in just one direction at a time.
--- 06-half-duplex-chat-tcp-ip/server.c
+++ 06-half-duplex-chat-tcp-ip/server.c
@@ -19,55 +19,41 @@ Thanks.
 //for gethostbyname
 #include "netdb.h"
 #include "arpa/inet.h"
-
+#define MAX 1000
 #define BACKLOG 5 // how many pending connections queue will hold
 int main()
 {
+    char serverMessage[MAX];
+    char clientMessage[MAX];
     //create the server socket
-    int server_descriptor;
- 
-  server_descriptor = socket(AF_INET, SOCK_STREAM, 0);
-    //define the server address
-    //creating the address as same way we have created for TCPclient
+    int  socketDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+    
 
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9002);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(9002);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
     //calling bind function to oir specified IP and port
-    if(bind(server_descriptor, (struct sockaddr*)&server_address, sizeof(server_address))){
-        perror("bind");
-    }
+    bind(socketDescriptor, (struct sockaddr*)&serverAddress, sizeof(serverAddress));    
 
-    listen(server_descriptor, BACKLOG);
+    listen(socketDescriptor, BACKLOG);
     
     //starting the accepting
-    int client_socket = accept(server_descriptor, NULL, NULL);
+    int clientSocketDescriptor = accept(socketDescriptor, NULL, NULL);
 
     while (1)
     {
-        char server_message[1000];
-        char client_message[1000];
         printf("\ntext message here .. :");
-        scanf("%s", server_message);
-        //accept(socketWeAreAccepting,structuresClientIsConnectingFrom,)
-        //sending data
-        //send(toWhom,Message,SizeOfMessage,FLAG);
-        send(client_socket, server_message, sizeof(server_message) , 0);
+        scanf("%s", serverMessage);
+        send(clientSocketDescriptor, serverMessage, sizeof(serverMessage) , 0);
         //recieve the data from the server
-        if (recv(client_socket, &client_message, sizeof(client_message), 0) != -1)
-        {
-            //recieved data from the server successfully then printing the data obtained from the server
-            printf("\nCLIENT: %s", client_message);
-        }
-        else
-        {
-            perror("recv");
-            exit(0);
-        }
+        recv(clientSocketDescriptor, &clientMessage, sizeof(clientMessage), 0) ;
+        //recieved data from the server successfully then printing the data obtained from the server
+        printf("\nCLIENT: %s", clientMessage);
+       
     }
         //close the socket
-        close(server_descriptor);
+        close(socketDescriptor);
         return 0;
 }
